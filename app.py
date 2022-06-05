9865d74ee77329b68b96170e5466a475e45912f7
unknown <pushpapraketh@gmail.com>
2022-06-06 00:09:14 +0530

    make it better

--- app.py
+++ app.py
@@ -93,5 +93,6 @@ def index():
        # return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(port=8000,debug=True) # running the app on the local machine on port 8000
+    #app.run(host='127.0.0.1', port=8000,debug=True) # running the app on the local machine on port 8000
+     app.run(debug=True)
 
