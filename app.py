69809976e6f920ac440532c211d0d72d78d5e1e9
unknown <pushpapraketh@gmail.com>
2022-06-06 00:20:22 +0530

    make it better

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ def index():
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8000,debug=True) # running the app on the local machine on port 8000
-     app.run(debug=True)
+    app.run(debug=True)
 
